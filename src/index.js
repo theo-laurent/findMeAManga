37dffcc8a20c60315a1f81840a768ddc15d16fd0
Theo Laurent <ze_king_to@hotmail.com>
2021-10-08 21:15:42 +0200

    p

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import App from "./App";
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter basename={process.env.PUBLIC_URL}>
+    <BrowserRouter>
       <App />
     </BrowserRouter>
   </React.StrictMode>,
